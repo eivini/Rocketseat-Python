9385e1bbc4499ba41bea16d37c670eccf4faa55d
Halphar <143683210+eivini@users.noreply.github.com>
2025-04-11 10:45:02 -0300

    Nível 01: Módulo 01, tipos de repetições

--- Nivel_01/Modulo_01/03_tipos_repeticoes/02_while.py
+++ Nivel_01/Modulo_01/03_tipos_repeticoes/02_while.py
@@ -4,14 +4,14 @@ while contador < 5:
     print("Contagem:", contador)
     contador += 1 # vale o mesmo que contador = contador + 1 / também funciona -= para subtração
 
-####
-# O break funciona de uma forma similar, apresentaria o mesmo resultado, mas não é recomendado
-# contador = 0
-# while True:
-#     print("Contagem:", contador)
-#     contador += 1
-#     if contador == 5:
-#         break
-####
+"""
+O break funciona de uma forma similar, apresentaria o mesmo resultado, mas não é recomendado
+contador = 0
+while True:
+    print("Contagem:", contador)
+    contador += 1
+    if contador == 5:
+        break
+"""
 
 print("Programa finalizado")
